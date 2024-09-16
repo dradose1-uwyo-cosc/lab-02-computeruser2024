1ef1205133a9e81db6806ffa1b36c30aae1460fa
computeruser2024 <eleyba@uwyo.edu>
2024-09-16 22:30:38 +0000

    finished lab 2

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -2,19 +2,17 @@
 # UWYO COSC 1010
 # 09/16/24
 # Lab 02 
-# Lab Section: 14
-None: 
+# Lab Section: 14None: 
 # your
 # comments
 # here
 
-python 3
-
-
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print("Hello, COSC1010")
+
+
 
 # Assign the string above to a variable named hello_message and print that variable
 hello_message = ""
@@ -26,24 +24,33 @@ print("Your code here")
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
+University="The University of Wyoming"
+year=1886
+print(f"The {University} was founded in {year}")
 
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+print(f"x + y = {5 + 10}")
+print(f"x - y = {5 - 10}")
+print(f"x * y = {5 * 10}")
+print(f"x / y = {5 / 10}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
     # String concatenation combines two strings together
     # It is done using the + operator
     # Create three variables:
-        # first_name, which is your first name 
+        # first_name which is your first name 
         # last_name, which is your last name
         # space, which is a space character 
     # Use string concatenation to print out your full name 
+first_name="Emma"
+last_name="Leyba"
+ 
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
